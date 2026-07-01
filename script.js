9325e64e9146062d870af9f4cdc7963088cae8d8
davitavaresoliveira-max <davi.tavares.oliveira@escola.pr.gov.br>
2026-07-01 19:30:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AgroTech</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+<h1>🌱 AgroTech</h1>
+<p>O Agro move o Brasil!</p>
+</header>
+
+<section class="banner">
+<img src="https://images.unsplash.com/photo-1500937386664-56d1dfef3854?w=1200" alt="Plantação">
+</section>
+
+<section class="conteudo">
+<h2>Sobre o Agronegócio</h2>
+
+<p>
+O agronegócio é um dos principais setores da economia brasileira. Ele produz alimentos,
+gera empregos e contribui para o desenvolvimento do país.
+</p>
+
+<button onclick="mostrarMensagem()">
+Saiba mais
+</button>
+
+<p id="mensagem"></p>
+
+</section>
+
+<footer>
+Desenvolvido para o trabalho de Programação
+</footer>
+
+<script src="script.js"></script>
+
+</body>
+</html>
